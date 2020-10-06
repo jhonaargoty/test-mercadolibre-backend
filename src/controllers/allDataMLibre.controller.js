a15eff19b71c8cc4f63405ddfaf2b94128b3fb4c
jhonaargoty <jhonatanargoty@gmail.com>
2020-10-06 16:50:36 -0500

    6Oct

--- src/controllers/allDataMLibre.controller.js
+++ src/controllers/allDataMLibre.controller.js
@@ -2,47 +2,48 @@ const axios = require("axios");
 
 exports.allData = async function allDataAxios(req, res) {
   const { q } = req.query;
-
-  const response = await axios.get(
-    "https://api.mercadolibre.com/sites/MLA/search?q=" + q
-  );
-  let allDataSearch = JSON.parse(JSON.stringify(response.data));
-
-  var itemsData = [];
-  var itemsCategories = [];
-
-  for (
-    var i = 0;
-    i < allDataSearch.filters[0].values[0].path_from_root.length;
-    i++
-  ) {
-    var obj = allDataSearch.filters[0].values[0].path_from_root[i];
-    itemsCategories.push(obj.name);
-  }
-  for (let i = 0; i <= 3; i++) {
-    itemsData.push({
-      id: allDataSearch.results[i].id,
-      title: allDataSearch.results[i].title,
-      price: {
-        currency: allDataSearch.results[i].currency_id,
-        amount: allDataSearch.results[i].price,
-        decimals: 0,
+  try {
+    const response = await axios.get(
+      "https://api.mercadolibre.com/sites/MLA/search?q=" + q
+    );
+    let allDataSearch = JSON.parse(JSON.stringify(response.data));
+    var itemsData = [];
+    var itemsCategories = [];
+    for (
+      var i = 0;
+      i < allDataSearch.filters[0].values[0].path_from_root.length;
+      i++
+    ) {
+      var obj = allDataSearch.filters[0].values[0].path_from_root[i];
+      itemsCategories.push(obj.name);
+    }
+    for (let i = 0; i <= 3; i++) {
+      itemsData.push({
+        id: allDataSearch.results[i].id,
+        title: allDataSearch.results[i].title,
+        price: {
+          currency: allDataSearch.results[i].currency_id,
+          amount: allDataSearch.results[i].price,
+          decimals: 0,
+        },
+        picture: allDataSearch.results[i].thumbnail,
+        condition: allDataSearch.results[i].condition,
+        free_shipping: allDataSearch.results[i].shipping.free_shipping,
+        address: allDataSearch.results[i].address.city_name,
+      });
+    }
+    let dataAllData = {
+      author: {
+        name: "Jhonatan",
+        lastname: "Argoty",
       },
-      picture: allDataSearch.results[i].thumbnail,
-      condition: allDataSearch.results[i].condition,
-      free_shipping: allDataSearch.results[i].shipping.free_shipping,
-      address: allDataSearch.results[i].address.city_name,
-    });
+      categories: itemsCategories,
+      items: itemsData,
+    };
+    res.json(dataAllData);
+    return res.status(200).send({});
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ error: "Error in the search" });
   }
-
-  let dataAllData = {
-    author: {
-      name: "Jhonatan",
-      lastname: "Argoty",
-    },
-    categories: itemsCategories,
-    items: itemsData,
-  };
-  res.json(dataAllData);
-  return res.status(200).send({});
 };
--- src/controllers/itemDataMLibre.controller.js
+++ src/controllers/itemDataMLibre.controller.js
@@ -2,37 +2,43 @@ const axios = require("axios");
 
 exports.itemMLibre = async function itemDataAxios(req, res) {
   const { id } = req.params;
+  try {
+    const response = await axios.get(
+      "https://api.mercadolibre.com/items/" + id
+    );
+    const responseDescr = await axios.get(
+      "https://api.mercadolibre.com/items/" + id + "/description"
+    );
 
-  const response = await axios.get("https://api.mercadolibre.com/items/" + id);
-  const responseDescr = await axios.get(
-    "https://api.mercadolibre.com/items/" + id + "/description"
-  );
+    let free_shipping = JSON.parse(JSON.stringify(response.data.shipping));
+    let pictureA = response.data.thumbnail.replace("/D_", "/D_NQ_NP_");
+    let picture = pictureA.replace("-I.jpg", "-O.jpg");
 
-  let free_shipping = JSON.parse(JSON.stringify(response.data.shipping));
-  let pictureA = response.data.thumbnail.replace("/D_", "/D_NQ_NP_");
-  let picture = pictureA.replace("-I.jpg", "-O.jpg");
-
-  let dataItem = {
-    author: {
-      name: "Jhonatan",
-      lastname: "Argoty",
-    },
-    item: {
-      id: response.data.id,
-      title: response.data.title,
-      price: {
-        currency: "response.data.currency_id",
-        amount: "response.data.price",
-        decimals: "OJO",
+    let dataItem = {
+      author: {
+        name: "Jhonatan",
+        lastname: "Argoty",
       },
-      picture: picture,
-      condition: response.data.condition,
-      sold_quantity: response.data.sold_quantity,
-      free_shipping: free_shipping.free_shipping,
-      sold_quantity: response.data.sold_quantity,
-      description: responseDescr.data.plain_text,
-    },
-  };
-  res.json(dataItem);
-  return res.status(200).send({});
+      item: {
+        id: response.data.id,
+        title: response.data.title,
+        price: {
+          currency: response.data.currency_id,
+          amount: response.data.price,
+          decimals: "OJO",
+        },
+        picture: picture,
+        condition: response.data.condition,
+        sold_quantity: response.data.sold_quantity,
+        free_shipping: free_shipping.free_shipping,
+        sold_quantity: response.data.sold_quantity,
+        description: responseDescr.data.plain_text,
+      },
+    };
+    res.json(dataItem);
+    return res.status(200).send({});
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ error: "Error in the search" });
+  }
 };
